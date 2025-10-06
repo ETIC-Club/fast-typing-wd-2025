8cf4a1e6b32f4bd8f17233a1db571e4b688a5571
Roufaida <mr_asbar@esi.dz>
2025-10-06 18:58:21 +0100

    last push

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/fast-typing-wd-2025',
-  images: {
-    unoptimized: true,
-  },
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
